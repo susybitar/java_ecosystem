5952c1a0bff7820cfe47331298ea08f3908cf427
susybitar <sbitarazevedo@futurespace.es>
2026-04-07 13:26:37 +0200

    feat: limpieza de proyecto antiguo, config de application.properties en spring_data y base de Entidad Libro

---
+++ spring_data/src/test/java/com/appsdeveloperblog/app/ws/futurespace/exercises/ExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.app.ws.futurespace.exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
