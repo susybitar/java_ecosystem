b5efb26d24504619118d3a02c8b27fc51e70d76a
susybitar <sbitarazevedo@futurespace.es>
2026-04-07 15:12:52 +0200

    refactor: recolocación de entidades en paquete entities y preparación de estructura para los repositorios

--- spring_data/src/main/java/com/appsdeveloperblog/app/ws/futurespace/exercises/ej02_jpa/entities/Author.java
+++ spring_data/src/main/java/com/appsdeveloperblog/app/ws/futurespace/exercises/ej02_jpa/entities/Author.java
@@ -1,4 +1,4 @@
-package com.appsdeveloperblog.app.ws.futurespace.exercises.ej02_jpa;
+package com.appsdeveloperblog.app.ws.futurespace.exercises.ej02_jpa.entities;
 
 import jakarta.persistence.*;
 
